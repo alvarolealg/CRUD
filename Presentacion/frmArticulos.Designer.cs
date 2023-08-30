45a419b93ce56a2ade5b44b642a04db9511b71ce
alvaro leal <lealgalvaro@gmail.com>
2023-08-31 00:33:23 +0200

    arregle el data grid view

--- Presentacion/frmArticulos.Designer.cs
+++ Presentacion/frmArticulos.Designer.cs
@@ -80,10 +80,13 @@
             // dgvArticulos
             // 
             this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
             this.dgvArticulos.Location = new System.Drawing.Point(13, 78);
             this.dgvArticulos.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvArticulos.MultiSelect = false;
             this.dgvArticulos.Name = "dgvArticulos";
             this.dgvArticulos.RowHeadersWidth = 51;
+            this.dgvArticulos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvArticulos.Size = new System.Drawing.Size(888, 363);
             this.dgvArticulos.TabIndex = 3;
             this.dgvArticulos.SelectionChanged += new System.EventHandler(this.dgvArticulos_SelectionChanged);
